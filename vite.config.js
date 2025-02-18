30f0c905a2e70878709ea540d67ab8527fb87876
Merimer08 <marialolu08@gmail.com>
2025-02-18 13:18:42 +0100

    fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pig-game-react-vite/', // Asegúrate de que esto coincida con el nombre de tu repositorio
   plugins: [react()],
 })
